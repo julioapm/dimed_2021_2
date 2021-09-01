0b8c68e50808119c42c284788872a6f1081ab8ac
Júlio Pereira Machado <julioapm@gmail.com>
2021-09-01 15:09:52 -0300

    Componente de navegação

--- demoforms/src/app/app-routing.module.ts
+++ demoforms/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'navegacao', component: NavegacaoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ demoforms/src/app/navegacao/navegacao.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navegacao',
+  templateUrl: './navegacao.component.html',
+  styleUrls: ['./navegacao.component.css']
+})
+export class NavegacaoComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
